d4529f824d3843e38a9499a3f3a38d651d42c6e4
phill720 <phillipschris902@gmail.com>
2016-09-21 13:24:40 -0400

    Lab 10 Remastered

--- Lab12/Program.cs
+++ Lab12/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Method_Library;
 
 namespace Lab12
 {
@@ -10,6 +11,52 @@ namespace Lab12
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter your name:\t");
+            string UserName = Console.ReadLine();
+            string opponent = "";
+            int wins = 0;
+            Console.Write("Would you like to play against Jimmy or Johns?:\t");
+            try
+            {
+                opponent = Console.ReadLine();
+
+                if (opponent.ToLower() == "jimmy" || opponent.ToLower() == "johns")
+                {
+                    Console.WriteLine("Select Rock, Paper, or Scissors:\t");
+                Weapon:
+                    string choice = Console.ReadLine().ToLower();
+                    if (choice == "rock" || choice == "paper" || choice == "scissors")
+                    {
+                        if (opponent.ToLower() == "jimmy")
+                        {
+                            wins += Class1.PlayJimmy(choice);
+                            Console.WriteLine(wins);
+                        }
+                        else
+                        {
+
+                            wins += Class1.PlayJohns(choice);
+                            Console.WriteLine(wins);
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"You cannot use {choice} in Roshambo. Choose again");
+                        goto Weapon;
+                    }
+                }
+
+                else
+                {
+                    throw new ArgumentException($"You are not able to play against {opponent}.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"You are not able to play against {opponent}.");
+                //throw new ArgumentException($"You are not able to play against {opponent}."); 
+            }
         }
     }
+    
 }
---
+++ Method Library/Class1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Library
+{
+    public class Class1
+    {
+        public static int PlayJimmy(string choice)
+        {
+            if (choice == "paper")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You beat Jimmy");
+                return +1;
+            }
+            else if (choice == "scissors")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("Draw!");
+                return +0;
+            }
+        }
+
+        List<string> RPS = new List<string> { "Rock", "Paper", "Scissors" };
+
+        public static int PlayJohns(string choice)
+        {
+            List<string> RPS = new List<string> { "Rock", "Paper", "Scissors" };
+            Random num = new Random (3);
+            int i = num.Next(3);
+            string JohnChoice = RPS[i];
+            Console.WriteLine("John:\t" + JohnChoice);
+            if (JohnChoice.ToLower() == "Rock" && choice == "Rock")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return + 0;
+            }
+            else if (JohnChoice.ToLower() == "Rock" && choice == "Paper")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else if (JohnChoice.ToLower() == "Rock" && choice == "Scissors")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else if (JohnChoice.ToLower() == "Paper" && choice == "Rock")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else if (JohnChoice.ToLower() == "Paper" && choice == "Paper")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else if (JohnChoice.ToLower() == "Paper" && choice == "Scissor")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else if (JohnChoice.ToLower() == "Scissor" && choice == "Rock")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else if (JohnChoice.ToLower() == "Scissor" && choice == "Paper")
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+            else 
+            {
+                Console.WriteLine("John:\tRock");
+                Console.WriteLine("You lost to Jimmy");
+                return +0;
+            }
+        }
+    }
+}
